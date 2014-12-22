0287d7ffc5f5553a8eb09261d4fc60d681326c38
Xenki <xetxaburu0001@ikasle.ehu.es>
2014-12-22 20:29:37 +0100

    Ganadutegia konpondua (?)

--- sanfermin2.0/src/administratzailea/GanadutegiaAldatu.java
+++ sanfermin2.0/src/administratzailea/GanadutegiaAldatu.java
@@ -24,7 +24,7 @@ public class GanadutegiaAldatu extends JFrame {
 	JLabel arduraduna = new JLabel("Arduradun berria:");
 	JLabel tlf = new JLabel("Telefono berria:");
 	JLabel helbide = new JLabel("Helbide berria:");
-	JComboBox arduradunBox;
+	JTextField arduradunaTestua = new JTextField(10);
 	JTextField tlfTestua = new JTextField(10);
 	JTextField helbideTestua = new JTextField(10);
 	JPanel behekoPanela = new JPanel();
@@ -39,9 +39,6 @@ public class GanadutegiaAldatu extends JFrame {
 
 	public GanadutegiaAldatu(int row) {
 		ganadutegiId = gk.getId().get(row);
-		Vector<String> v = ErabiltzaileKudeatzailea.getInstantzia().getIzenak();
-		v.add("--------");
-		arduradunBox = new JComboBox(v);
 		gridBagHasieratu();
 	}
 
@@ -61,7 +58,7 @@ public class GanadutegiaAldatu extends JFrame {
 		gehituOsagaia(helbide, 4, 1, 5, 1);
 		mugak.insets = new Insets(3, 3, 3, 3);
 
-		gehituOsagaia(arduradunBox, 2, 5, 3, 1);
+		gehituOsagaia(arduradunaTestua, 2, 5, 3, 1);
 		mugak.insets = new Insets(3, 3, 3, 3);
 		gehituOsagaia(tlfTestua, 3, 5, 5, 1);
 		mugak.insets = new Insets(3, 3, 3, 3);
@@ -75,9 +72,9 @@ public class GanadutegiaAldatu extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (!arduradunBox.getSelectedItem().toString().equals("--------")){
+				if (!arduradunaTestua.getText().equals("")){
 					String izenZaharra = AukeraAdmin.getInstantzia().gtm.getValueAt(AukeraAdmin.getInstantzia().tableGanadutegiak.getTable().getSelectedRow(),1).toString();
-					String izenBerria = arduradunBox.getSelectedItem().toString();
+					String izenBerria = arduradunaTestua.getText();
 					
 					ErabiltzaileKudeatzailea.getInstantzia().izenaAldatu(izenZaharra, izenBerria);
 				}if(!tlfTestua.getText().equals("")){
